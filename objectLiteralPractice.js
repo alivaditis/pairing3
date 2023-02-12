91b3cd7d12c202e32951465521d575539ec8b459
Alec Livaditis <aleclivaditis@gmail.com>
2023-02-12 14:50:43 -0500

    Add function version

--- objectLiteralPractice.js
+++ objectLiteralPractice.js
@@ -1,3 +1,11 @@
 var numbers = [2, 17, 25, 32, 45, 52, 69, 72];
 
 console.log(numbers.filter(number => number > 30));
+
+function isSmallEnough(value) {
+    return value < 60;
+}
+
+var filter2 = numbers.filter(isSmallEnough);
+
+console.log(filter2);
